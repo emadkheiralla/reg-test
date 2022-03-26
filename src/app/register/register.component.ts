5b0623484db4894ec003cb254d674e94178adef8
emadkheiralla <emadkheiralla@hotmail.com>
2022-03-26 01:29:33 -0400

    Added form validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
@@ -14,20 +14,22 @@ export class RegisterComponent implements OnInit {
   @Input()
   error!: string | null;
   form: FormGroup = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    bio: new FormControl(''),
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    bio: new FormControl('', Validators.required)
   });
   url = 'https://mocki.io/v1/7f434df6-a4ac-4817-ab7c-dd39a564d01d';
+  submitted = false;
 
-  constructor(private http: HttpClient, private router: Router, private user: UserService) { }
+  constructor(private http: HttpClient, private router: Router, private user: UserService) {}
 
   ngOnInit(): void {
     
   }
 
   submit() {
+    this.submitted = true;
     if (this.form.valid) {
       this.http.get(this.url).subscribe((data: any) => {
         if(data.success){
